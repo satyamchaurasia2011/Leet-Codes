34b74b076ab9cc334e56030fdad53d0164d8adf2
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-02-07 12:36:49 +0530

    Time: 16 ms (90.65%), Space: 16.8 MB (99.03%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int>mp;
+       //  map<int, int>mp;
+       //  for(auto &&a : nums)
+       //      mp[a]++;
+       // for(auto &&a : nums)
+       //     if(mp[a]==1)
+       //         return a;
+        //or
+        if(nums.size()==1) return nums[0];
+        int res=0;
         for(auto &&a : nums)
-            mp[a]++;
-       for(auto &&a : nums)
-           if(mp[a]==1)
-               return a;
-        return 0;
+            res = res^a;
+        
+        return res;
     }
 };
