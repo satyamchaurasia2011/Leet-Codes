fb47db88d4097fc0cc2dc797abf4923042b3f71a
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-02-17 12:00:12 +0530

    Time: 32 ms (34.77%), Space: 19.4 MB (28.19%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    vector<vector<int>>comb;
+    void dfs(vector<int>& candidates, int i, int total, int target, vector<int>& temp){
+        if(total == target){
+            vector<int>copy = temp;
+            comb.push_back(copy);
+            return;
+        }
+        else if(i >= candidates.size() || total > target)
+            return;
+         temp.push_back(candidates[i]);
+        dfs(candidates, i, total+candidates[i], target, temp);
+        
+        temp.pop_back();
+        dfs(candidates, i+1, total, target, temp);
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        dfs(candidates, 0, 0, target, temp);
+            return comb;
+        
+    }
+};
