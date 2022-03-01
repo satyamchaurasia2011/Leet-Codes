fdf1c4f86090b67e6bb9b97181d60ab815ec7aa6
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-01 11:40:26 +0530

    Time: 6 ms (65.69%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,15 +1,27 @@
 class Solution {
+    int count(int n, vector<int>& memo){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        if(memo[n] != 0)
+            return memo[n];
+        if(n%2 == 0){
+            memo[n] = count(n/2, memo);
+            return count(n/2, memo);
+        }
+        else{
+            memo[n] = 1 + count(n/2, memo);
+            return (1 + count(n/2, memo));
+        }
+    }
 public:
     vector<int> countBits(int n) {
-        vector<int>vec;
+        vector<int>memo(n+1);
         for(int i=0; i<=n; i++)
         {
-            int count = 0;
-            for(int j=0; j<32; j++)
-                if(i&(1<<j))
-                    count++;
-            vec.push_back(count);
+           memo[i] = count(i, memo);
         }
-        return vec;
+        return memo;
     }
 };
