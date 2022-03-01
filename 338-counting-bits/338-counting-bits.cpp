130414ba0712dfdaa80b6d7ea7ae8ed5d7144de7
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-01 11:42:13 +0530

    Time: 7 ms (62.32%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,27 +1,33 @@
 class Solution {
-    int count(int n, vector<int>& memo){
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
-        if(memo[n] != 0)
-            return memo[n];
-        if(n%2 == 0){
-            memo[n] = count(n/2, memo);
-            return count(n/2, memo);
-        }
-        else{
-            memo[n] = 1 + count(n/2, memo);
-            return (1 + count(n/2, memo));
-        }
-    }
+    // int count(int n, vector<int>& memo){
+    //     if(n == 0)
+    //         return 0;
+    //     if(n == 1)
+    //         return 1;
+    //     if(memo[n] != 0)
+    //         return memo[n];
+    //     if(n%2 == 0){
+    //         memo[n] = count(n/2, memo);
+    //         return count(n/2, memo);
+    //     }
+    //     else{
+    //         memo[n] = 1 + count(n/2, memo);
+    //         return (1 + count(n/2, memo));
+    //     }
+    // }
 public:
     vector<int> countBits(int n) {
-        vector<int>memo(n+1);
-        for(int i=0; i<=n; i++)
-        {
-           memo[i] = count(i, memo);
+        // vector<int>memo(n+1);
+        // for(int i=0; i<=n; i++)
+        // {
+        //    memo[i] = count(i, memo);
+        // }
+        // return memo;
+        vector<int> res(n+1);
+        res[0] = 0;
+        for(int i=1; i<=n; i++){
+            res[i] = res[i/2] + i%2;
         }
-        return memo;
+        return res;
     }
 };
