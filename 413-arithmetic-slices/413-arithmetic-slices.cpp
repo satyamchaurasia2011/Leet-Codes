7b3b91bd53387fffc6a712b8828bd2c293b6502e
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-03 13:35:02 +0530

    Time: 4 ms (49.49%), Space: 7.4 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+    ///AP series 
+    // |2|5|9|12|15|18|22|26|30|34||36|38|40|41|
+    // first 2 is no ap then check 3 element's A.P if yes then add +1 with previous if there is already ap.
+    // |0|0|0|0|1|2|0|1|2|3|0|1|2|0| add all element  = 12 -ans
     int numberOfArithmeticSlices(vector<int>& nums) {
         vector<int>vec(nums.size());
         if(nums.size()<2)
