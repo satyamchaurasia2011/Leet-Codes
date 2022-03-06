8d9648260659465b3f840d4f0b5820e6bc1a6605
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-06 12:47:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.70%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,10 @@
+typedef long long ll;
+class Solution {
+public:
+    int countOrders(int n) {
+       long long dp = 1;
+        for(int i=2; i<=n; i++)
+            dp = dp * (i * (2*i -1)) % 1000000007;
+        return dp;
+    }
+};
