9d5cde69005c5f0da0c9199e4fec799a218f777d
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-08 20:30:59 +0530

    Time: 12 ms (77.92%), Space: 7.9 MB (96.13%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,11 +9,22 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        while(head){
-            if(head->next && head->next->val == 100001)
-                return true;
-            head->val = 100001;
-            head = head->next;
+        //jugaad
+        // while(head){
+        //     if(head->next && head->next->val == 100001)
+        //         return true;
+        //     head->val = 100001;
+        //     head = head->next;
+        // }
+        //return false;
+        
+        //two pointers
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
         }
         return false;
     }
