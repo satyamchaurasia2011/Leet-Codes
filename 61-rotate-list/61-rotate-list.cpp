12917309fc46e522e93c1c387caaa4b65f789f5d
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-11 12:23:30 +0530

    Time: 13 ms (37.97%), Space: 11.8 MB (55.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -11,33 +11,51 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next) return head;
-        int size=0;
-        ListNode* p;
-        p = head;
-        while(p){
+        // if(!head || !head->next) return head;
+        // int size=0;
+        // ListNode* p;
+        // p = head;
+        // while(p){
+        //     size++;
+        //     p = p->next;
+        // }
+        // if(size <= k)
+        //     k = k % size;
+        // if(k == 0)
+        //     return head;
+        // int counter=0;
+        // ListNode* s = NULL;
+        // ListNode* f = head;
+        // while(counter != size-k){
+        //     s = f;
+        //     f = f->next;
+        //     counter++;
+        // }
+        // s->next = NULL;
+        // s = f;
+        // while(f->next){
+        //     f = f->next;
+        // }
+        // f->next = head;
+        // head = s;
+        // return head;
+        
+        //More optimise and clean code --
+        if(!head) return head;
+        int size=1;
+        ListNode *newH, *tail;
+        newH = tail = head;
+        while(tail->next){
             size++;
-            p = p->next;
+            tail = tail->next;
         }
-        if(size <= k)
-            k = k % size;
-        if(k == 0)
-            return head;
-        int counter=0;
-        ListNode* s = NULL;
-        ListNode* f = head;
-        while(counter != size-k){
-            s = f;
-            f = f->next;
-            counter++;
+        tail->next = head;
+        if(k %= size){
+            for(int i=0; i<size-k; i++)
+                tail = tail->next;
         }
-        s->next = NULL;
-        s = f;
-        while(f->next){
-            f = f->next;
-        }
-        f->next = head;
-        head = s;
-        return head;
+         newH = tail->next;
+         tail->next = NULL;
+        return newH;
     }
 };
