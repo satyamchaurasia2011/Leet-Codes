a49c4fc13f5c7b30cb7833cef583bf544956a0af
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-03-21 20:43:44 +0530

    Time: 164 ms (67.79%), Space: 111.6 MB (29.11%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-       vector<int>top_occ(7, 0);
-        vector<int>bottom_occ(7, 0);
+       vector<int> A(7, 0), B(7, 0), same(7, 0);
         for(int i=0; i<tops.size(); i++)
         {
-            top_occ[tops[i]]++;
-            bottom_occ[bottoms[i]]++;
+            A[tops[i]]++;
+            B[bottoms[i]]++;
+            if(tops[i] == bottoms[i])
+                same[tops[i]]++;
         }
-        int t_ind =0, b_ind = 0;
-        for(int i=0; i<top_occ.size(); i++){
-            if(top_occ[i] > top_occ[t_ind])
-                t_ind = i;
-        }
-           
-         for(int i=0; i<bottom_occ.size(); i++){
-            if(bottom_occ[i] > bottom_occ[b_ind])
-                b_ind = i;
-        }
-        if(top_occ[t_ind] > bottom_occ[b_ind]){
-            for(int i=0; i<tops.size(); i++)
-                if(tops[i] != t_ind && bottoms[i] != t_ind)
-                    return -1;
-            return tops.size() - top_occ[t_ind];
-           }
-                
-        else {
-            for(int i=0; i<bottoms.size(); i++)
-                if(bottoms[i] != b_ind && tops[i] != b_ind)
-                    return -1;
-              return tops.size() - bottom_occ[b_ind];
-        }
+        for(int i=0; i<7; i++)
+            if(A[i] + B[i] - same[i] == tops.size())
+                return tops.size() - max(A[i], B[i]);
+        
         return -1;
     }
 };
