ae5db1306753afa8e82a7b687ba1bc6a4f583370
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-05-05 12:40:19 +0530

    Time: 5 ms (21.37%), Space: 6.3 MB (65.90%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,38 +1,73 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-       stack<char>stk1;
-        stack<char>stk2;
-        int i=0;
-        while(i < s.length())
+    //    stack<char>stk1;
+    //     stack<char>stk2;
+    //     int i=0;
+    //     while(i < s.length())
+    //     {
+    //         if(s[i] != '#')
+    //             stk1.push(s[i]);
+    //         else if(!stk1.empty())
+    //             stk1.pop();
+    //         i++;
+    //     }
+    //     string s1 = " ";
+    //     while(!stk1.empty())
+    //     {
+    //         s1 += stk1.top();
+    //         stk1.pop();
+    //     }
+    //     i=0;
+    //     while(i < t.length())
+    //     {
+    //         if(t[i] != '#')
+    //             stk2.push(t[i]);
+    //         else if(!stk2.empty())
+    //             stk2.pop();
+    //         i++;
+    //     }
+    //     string s2 = " ";
+    //     while(!stk2.empty())
+    //     {
+    //         s2 += stk2.top();
+    //         stk2.pop();
+    //     }
+    //     return s1 == s2;
+    // 
+    
+        int k=0, p=0;
+        for(int i=0; i<s.length(); i++)
         {
-            if(s[i] != '#')
-                stk1.push(s[i]);
-            else if(!stk1.empty())
-                stk1.pop();
-            i++;
+            if(s[i] == '#'){
+                k--;
+                k = max(0, k);
+            }
+            else{
+                s[k] = s[i];
+                k++;
+            }
         }
-        string s1 = " ";
-        while(!stk1.empty())
+        for(int i=0; i<t.length(); i++)
         {
-            s1 += stk1.top();
-            stk1.pop();
+            if(t[i] == '#'){
+                p--;
+                p = max(0, p);
+            }
+            else{
+                t[p] = t[i];
+                p++;
+            }
         }
-        i=0;
-        while(i < t.length())
-        {
-            if(t[i] != '#')
-                stk2.push(t[i]);
-            else if(!stk2.empty())
-                stk2.pop();
-            i++;
+        if(k != p)
+            return false;
+        else{
+            for(int i=0; i<k; i++)
+                if(s[i] != t[i])
+                    return false;
+            return true;
         }
-        string s2 = " ";
-        while(!stk2.empty())
-        {
-            s2 += stk2.top();
-            stk2.pop();
-        }
-        return s1 == s2;
+    
+    
     }
 };
