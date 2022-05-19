63e452572bccdaaa37a2fbfe473f24cd93a9844c
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-05-19 21:14:14 +0530

    Time: 32 ms (5.58%), Space: 17.6 MB (5.75%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int color = -1;
+    int pre = -1;
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        if(color == -1) color = image[sr][sc];
-        if(image.empty() || sr == -1 || sr == image.size() || sc == -1
-           || sc == image[0].size() || image[sr][sc] != color || color == newColor) 
+        if(pre == -1) pre = image[sr][sc];
+        if(sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != pre || image[sr][sc] == newColor)
             return image;
+        
         image[sr][sc] = newColor;
-        floodFill(image, sr - 1, sc, newColor);
-        floodFill(image, sr + 1, sc, newColor);
-        floodFill(image, sr, sc - 1, newColor);
-        floodFill(image, sr, sc + 1, newColor);
+           
+        floodFill(image, sr-1, sc, newColor);
+        floodFill(image, sr, sc-1, newColor);
+        floodFill(image, sr+1, sc, newColor);
+        floodFill(image, sr, sc+1, newColor);
         return image;
     }
 };
