0ce8dd0302585930ade5f4c0ec9932c899300ad7
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-05-21 22:09:47 +0530

    Time: 8 ms (22.60%), Space: 7.8 MB (41.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -7,9 +7,7 @@ public:
         if(obstacleGrid[m-1][n-1] == 0) path[m-1][n-1] = 1;
         for(int i = m-1; i>=0; i--)
         {
-            if(obstacleGrid[i][n-1] == 0)
-              path[i][n-1] += path[i+1][n-1];
-            for(int j = n-2; j>=0; j--)
+            for(int j = n-1; j>=0; j--)
                 if(obstacleGrid[i][j] == 0)
                         path[i][j] += path[i+1][j] + path[i][j+1];
                 
