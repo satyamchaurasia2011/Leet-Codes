fbc380518a90485c833f1470b64d17d6b29df30d
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-06-09 09:40:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (27.23%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -4,9 +4,9 @@ public:
         int i=0, j = s.length()-1;
         while(i < j-i)
         {
-            if(s[i] == s[j-i])
-                i++;
-            else return 2;
+            if(s[i] != s[j-i])
+              return 2;
+            i++;
         }
         return 1;
     }
