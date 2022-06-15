6bace9fe14d39889ff239310148a33d9eaf2e198
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-06-15 22:53:05 +0530

    Time: 12 ms (29.26%), Space: 9 MB (21.65%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,22 +1,19 @@
 class Solution {
    
 public:
-     int min_sum(vector<vector<int>>&triangle, int i, int j, vector<vector<int>>&dp)
-    {
-        if(i == triangle.size()-1){
-            dp[i][j] = triangle[i][j];
-           return triangle[i][j];
-        }
-        if(dp[i][j] != INT_MAX)
-            return dp[i][j];
-        int left = min_sum(triangle, i+1, j, dp);
-        int right = min_sum(triangle, i+1, j+1, dp);
-         dp[i][j] = min(left, right)+triangle[i][j];
-        return dp[i][j];
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), INT_MAX));
-       return min_sum(triangle, 0, 0, dp);
-       
+        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), 0));
+       for(int i=triangle.size()-1; i>=0; i--)
+       {
+           for(int j=0; j<triangle[i].size(); j++)
+           {
+               if(i == triangle.size()-1)
+                   dp[i][j] = triangle[i][j];
+               else{
+                   dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+               }
+           }
+       }
+        return dp[0][0];
     }
 }; 
