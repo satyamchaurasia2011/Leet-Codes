6a060c49ea83fa67fa28d35bcca0df548329db3a
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-06-22 10:05:20 +0530

    Time: 13 ms (50.95%), Space: 10.1 MB (37.94%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
-    int partioning(vector<int> &nums, int p, int r)
-    {
-        int x = nums[r];
-        int i = p-1;
-        for(int j = p; j<r; j++)
-        {
-            if(nums[j] <= x){
-                i++;
-                swap(nums[i], nums[j]);
-            }
-        }
-         swap(nums[i+1], nums[r]);
-        return i+1;
-    }
-    int quickSelect(vector<int>& nums, int p, int r, int k)
-    {
-        if(p == r)
-            return nums[p];
+//     int partioning(vector<int> &nums, int p, int r)
+//     {
+//         int x = nums[r];
+//         int i = p-1;
+//         for(int j = p; j<r; j++)
+//         {
+//             if(nums[j] <= x){
+//                 i++;
+//                 swap(nums[i], nums[j]);
+//             }
+//         }
+//          swap(nums[i+1], nums[r]);
+//         return i+1;
+//     }
+//     int quickSelect(vector<int>& nums, int p, int r, int k)
+//     {
+//         if(p == r)
+//             return nums[p];
         
-       int q = partioning(nums, p, r);   
-      
-        // int v = q-p+1;
-        if(q == k) return nums[q];
-        else if(k < q)
-            return quickSelect(nums, p, q-1, k);
-        else 
-            return quickSelect(nums, q+1, r, k);
+//        int q = partioning(nums, p, r);   
+//         if(q == k) return nums[q];
+//         else if(k < q)
+//             return quickSelect(nums, p, q-1, k);
+//         else 
+//             return quickSelect(nums, q+1, r, k);
         
-    }
+//     }
     
     int findKthLargest(vector<int>& nums, int k) {
         
        
-       return quickSelect(nums, 0, nums.size()-1, nums.size()-k);
+       // return quickSelect(nums, 0, nums.size()-1, nums.size()-k);
+        
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i=0; i<k; i++)
+            pq.push(nums[i]);
+        for(int i=k; i<nums.size(); i++)
+        {
+            if(nums[i] > pq.top())
+            {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
         
     }
 };
