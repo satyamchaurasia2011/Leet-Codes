ea1c4370d097b54db0fcf9f0981c994a01f3539d
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-06-30 20:28:13 +0530

    Time: 11 ms (53.82%), Space: 7.2 MB (88.14%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
 
-    bool isQueenSafe(vector<vector<int>>chess, int row, int col){
+    bool isQueenSafe(vector<string>&board, int row, int col, int n){
         for(int i=row-1, j = col; i>=0; i--)
-            if(chess[i][j] == 1)
+            if(board[i][j] == 'Q')
                 return false;
         for(int i=row-1, j=col-1; i>=0 && j>=0; i--,j--)
-            if(chess[i][j] == 1)
+            if(board[i][j] == 'Q')
                 return false;
-        for(int i=row-1, j=col+1; i>=0 && j<chess.size(); i--,j++)
-            if(chess[i][j] == 1)
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--,j++)
+            if(board[i][j] == 'Q')
                 return false;
         
         return true;
     }
     
-    void nQueens(vector<vector<int>>chess, vector<vector<string>>&ans,vector<string>&board, int row){
-        if(row == chess.size()){
+    void nQueens(vector<vector<string>>&ans,vector<string>&board, int row, int n){
+        if(row == n){
             ans.push_back(board);
             return;
         }
-        for(int col = 0; col<chess.size(); col++){
-            if(isQueenSafe(chess, row, col)){
-            chess[row][col] = 1;
+        for(int col = 0; col<n; col++){
+            if(isQueenSafe(board, row, col, n)){
             board[row][col] = 'Q';
-            nQueens(chess, ans, board, row+1);
+            nQueens(ans, board, row+1, n);
              board[row][col] = '.';
-            chess[row][col] = 0;
         }
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>>chess(n, vector<int>(n, 0));
         vector<vector<string>>ans;
         vector<string>board(n);
         string str(n, '.');
         for(int i=0; i<n; i++)
             board[i] = str;
-        nQueens(chess, ans, board, 0);
+        nQueens(ans, board, 0,n);
         return ans;
     }
 };
