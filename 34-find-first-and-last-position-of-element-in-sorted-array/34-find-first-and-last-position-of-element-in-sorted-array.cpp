981c426470c55bb2567ed82296d0fad14bde9983
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-07-25 18:42:52 +0530

    Time: 15 ms (39.90%), Space: 13.7 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-//         int left = INT_MAX, right = -1;
+    int getPosition(vector<int>&nums, int target, bool isLeft){
+         int i = 0, pos = -1;
+            int j = nums.size()-1;
+            while(i <= j){
+                int mid = (i+j)/2;
+                if(nums[mid] == target){
+                    pos = mid;
+                    if(isLeft) j = mid-1;
+                    else i = mid+1;
+                }
+                else if(nums[mid] > target)
+                     j = mid-1;
+                else
+                     i = mid+1;
+            }
+        return pos;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {        
+    int left = getPosition(nums, target, true);
+        if(left == -1) return vector<int>{-1, -1};
+    int right = getPosition(nums, target, false);
+        return vector<int>{left, right};
+        
+        
+        //         int left = INT_MAX, right = -1;
 //         while(true){
 //             int i = 0;
 //             int j = nums.size()-1;
@@ -31,14 +54,15 @@ public:
 //         if(left == INT_MAX) left = -1;
 //         return vector<int>{left, right};
         //2nd soln
-        int left = -1, right = -1;
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i] == target && left == -1)
-               left = right = i;                
-            else if(nums[i] == target && (i+1>=nums.size() || nums[i+1] != target))
-                right = i;
-        }
-        return vector<int>{left, right};
+    //     int left = -1, right = -1;
+    //     for(int i=0; i<nums.size(); i++)
+    //     {
+    //         if(nums[i] == target && left == -1)
+    //            left = right = i;                
+    //         else if(nums[i] == target && (i+1>=nums.size() || nums[i+1] != target))
+    //             right = i;
+    //     }
+    //     return vector<int>{left, right};
+    // }
     }
 };
