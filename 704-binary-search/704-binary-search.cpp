bcdd95f56f41707ab85e10fb4f00f0d5e0cd4546
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-09-22 00:36:11 +0530

    Time: 124 ms (5.25%), Space: 27.5 MB (63.18%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    int findNum(vector<int>&nums, int target, int s, int e){
+        if(s > e)
+            return -1;
+        int mid = (s+e)/2;
+        if(target == nums[mid])
+            return mid;
+        else if(target < nums[mid])
+            return findNum(nums, target, s, mid-1);
+        return findNum(nums, target, mid+1, e);
+    }
     int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1;
-        while(l <= h)
-        {
-             int mid = (l+h)/2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] <= target)
-                l=mid+1;
-            else 
-                h = mid-1;
-        }
-        return -1;
+        return findNum(nums, target, 0, nums.size()-1);
     }
 };
