aced287e6882fb932e568613b5e35654ab7e8030
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-09-30 00:08:46 +0530

    Time: 27 ms (5.60%), Space: 13.7 MB (19.04%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,68 +1,28 @@
 class Solution {
 public:
-    int getPosition(vector<int>&nums, int target, bool isLeft){
-         int i = 0, pos = -1;
-            int j = nums.size()-1;
-            while(i <= j){
-                int mid = (i+j)/2;
-                if(nums[mid] == target){
-                    pos = mid;
-                    if(isLeft) j = mid-1;
-                    else i = mid+1;
-                }
-                else if(nums[mid] > target)
-                     j = mid-1;
-                else
-                     i = mid+1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size()-1;
+        int idx = -1;
+        while(i <= j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] == target){
+                idx = mid;
+                break;
             }
-        return pos;
-    }
-    vector<int> searchRange(vector<int>& nums, int target) {        
-    int left = getPosition(nums, target, true);
-        if(left == -1) return vector<int>{-1, -1};
-    int right = getPosition(nums, target, false);
-        return vector<int>{left, right};
-        
-        
-        //         int left = INT_MAX, right = -1;
-//         while(true){
-//             int i = 0;
-//             int j = nums.size()-1;
-//             int mid;
-//              int isFound = false;
-//             while(i <= j){
-//                 mid = (i+j)/2;
-//                 if(nums.size() == 0)break;
-//                 if(nums[mid] == target){
-//                     if(mid < left)
-//                         left = mid;
-//                     if(mid > right)
-//                         right = mid;
-//                     isFound = true;
-//                     i = mid+1;
-//                 }
-//                 else if(nums[mid] < target)
-//                     i = mid+1;
-//                 else
-//                     j = mid-1;
-                
-//             }
-//             if(isFound == false || nums.size() == 0)
-//                 break;
-//             nums.erase(nums.begin()+mid);
-//         }
-//         if(left == INT_MAX) left = -1;
-//         return vector<int>{left, right};
-        //2nd soln
-    //     int left = -1, right = -1;
-    //     for(int i=0; i<nums.size(); i++)
-    //     {
-    //         if(nums[i] == target && left == -1)
-    //            left = right = i;                
-    //         else if(nums[i] == target && (i+1>=nums.size() || nums[i+1] != target))
-    //             right = i;
-    //     }
-    //     return vector<int>{left, right};
-    // }
+            else if(nums[mid] < target)
+                i = mid+1;
+            else
+                j = mid-1;
+        }
+        if(idx == -1)
+            return vector<int>{-1, -1};
+        int left = idx;
+        int right = idx;
+        while(left >= 0 && nums[left] == target)
+            left--;
+        while(right < nums.size() && nums[right] == target)
+            right++;
+        return vector<int>{left+1, right-1};
     }
 };
