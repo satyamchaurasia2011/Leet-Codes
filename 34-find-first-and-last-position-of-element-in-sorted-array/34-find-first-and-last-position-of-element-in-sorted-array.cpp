5ec73cd2ba5f4a31b786ef1b94d09b6f1149bd08
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-10-03 16:04:04 +0530

    Time: 28 ms (6.24%), Space: 13.5 MB (84.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int i = 0;
-        int j = nums.size()-1;
-        int idx = -1;
+        // int i = 0;
+        // int j = nums.size()-1;
+        // int idx = -1;
+        // while(i <= j){
+        //     int mid = i + (j-i)/2;
+        //     if(nums[mid] == target){
+        //         idx = mid;
+        //         break;
+        //     }
+        //     else if(nums[mid] < target)
+        //         i = mid+1;
+        //     else
+        //         j = mid-1;
+        // }
+        // if(idx == -1)
+        //     return vector<int>{-1, -1};
+        // int left = idx;
+        // int right = idx;
+        // while(left >= 0 && nums[left] == target)
+        //     left--;
+        // while(right < nums.size() && nums[right] == target)
+        //     right++;
+        // return vector<int>{left+1, right-1};
+        
+        int i=0, j=nums.size()-1;
+        int start = -1, end = -1;
         while(i <= j){
-            int mid = i + (j-i)/2;
+            int mid = (i+j)/2;
             if(nums[mid] == target){
-                idx = mid;
-                break;
+                start = mid;
+                j = mid-1;
             }
             else if(nums[mid] < target)
                 i = mid+1;
             else
                 j = mid-1;
         }
-        if(idx == -1)
-            return vector<int>{-1, -1};
-        int left = idx;
-        int right = idx;
-        while(left >= 0 && nums[left] == target)
-            left--;
-        while(right < nums.size() && nums[right] == target)
-            right++;
-        return vector<int>{left+1, right-1};
+        i=0;
+        j = nums.size()-1;
+        while(i <= j){
+            int mid = (i+j)/2;
+            if(nums[mid] == target){
+                end = mid;
+                i = mid+1;
+            }
+            else if(nums[mid] < target)
+                i = mid+1;
+            else
+                j = mid-1;
+        }
+        if(start != -1 && end == -1)
+            return vector<int>{start, start};
+        return vector<int>{start, end};
     }
 };
