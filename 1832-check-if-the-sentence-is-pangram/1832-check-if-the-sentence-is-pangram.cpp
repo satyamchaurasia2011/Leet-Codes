67efed7fed95edaff8f3f764a08f156a4d53e8aa
Satyam Chaurasia <54991670+satyamchaurasia2011@users.noreply.github.com>
2022-10-17 18:07:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<int>freq(26, 0); int count=0;
-        for(int i=0; i<sentence.size(); i++){
-            if(freq[sentence[i] - 'a'] == 0)
-                count++;
-             freq[sentence[i] - 'a']++;
-        }
-       return count == 26;
+        vector<int>mp(26, 0);
+        for(int i=0; i<sentence.size(); i++)
+            mp[sentence[i] - 'a']++;
+        for(int i=0; i<26; i++)
+            if(mp[i] == 0)
+                return false;
+        return true;
     }
 };
